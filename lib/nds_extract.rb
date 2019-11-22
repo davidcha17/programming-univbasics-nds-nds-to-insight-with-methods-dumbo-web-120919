ba11e914e3170bb422bfcdef83de9f5a304e8f1f
David Cha <david.cha44@qmail.cuny.edu>
2019-11-22 21:41:13 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,11 +9,11 @@ require 'directors_database'
 def directors_totals(nds)
   result = {}
   director_index = 0
-    while director_index < nds.size do
-      director = nds[director_index]
-      result[director][:name] = gross_for_director(director)
-      director_index += 1
-    end
+  while director_index < nds.size do
+    director = nds[director_index]
+    result[director[:name]] = gross_for_director(director)
+    director_index += 1
+  end
   result
 end
 
